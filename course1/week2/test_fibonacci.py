7b40d99d7be671c85a3bcd4e74607f31df252a91
tobias <leongtobias@gmail.com>
2020-09-20 17:13:32 +0800

    Add fibonacci

--- course1/week2/test_fibonacci.py
+++ course1/week2/test_fibonacci.py
@@ -1,8 +1,14 @@
-from fibonacci import naive_fib
+from fibonacci import naive_fib # noqa 
 
 
-def test_fib_small():
+def test_naive_fib_small():
     assert naive_fib(3) == 2
     assert naive_fib(2) == 1
     assert naive_fib(1) == 1
     assert naive_fib(0) == 0
+
+
+def test_naive_fib_large():
+    # assert naive_fib(50) == 12586269025
+    assert naive_fib(20) == 6765
+    assert naive_fib(10) == 55
