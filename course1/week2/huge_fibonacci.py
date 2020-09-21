f70b5d0945b5e457808b4ea99a7b4654a79613b4
tobias <tobias@dsaid.gov.sg>
2020-09-22 00:51:03 +0800

    Add docstrings for huge_fibonacci.py

--- course1/week2/huge_fibonacci.py
+++ course1/week2/huge_fibonacci.py
@@ -1,18 +1,42 @@
 def get_mod_from_fibo(n, m):
+    """
+    Returns the value of the n-th Fibonacci number mod by m.
+
+    Args:
+        n (int): n-th Fibonacci number
+        m (int): mod
+
+    Returns:
+        int: Value of n-th Fibonacci number mod by m
+    """
     pisano = PisanoPeriod(m)
     num = n % pisano.get_period_length()
     return pisano.period[num]
 
 
 class PisanoPeriod:
+    """
+    Class for calculating a pisano period.
+    """
+
     def __init__(self, m):
+        """
+        Initialises period to hold m and the calculated period.
+
+        Args:
+            m (int): mod factor
+        """
         self.m = m
         self._derive_period()
 
     def _derive_period(self):
+        """
+        Derives period for given m.
+        """
         fib_nums = []
         period = []
 
+        # initialise arrays to cover first two iterations
         fib_nums.append(0)
         fib_nums.append(1)
         period.append(0)
@@ -29,6 +53,12 @@ class PisanoPeriod:
         self.period = period[:-2]
 
     def get_period_length(self):
+        """
+        Returns length of calculated period.
+
+        Returns:
+            int: Length of calculated period
+        """
         return len(self.period)
 
 
